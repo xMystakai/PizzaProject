9cfd6eaa4d4a796cb02019043d4144322181b9e8
Zachary White <zacharywhite@maed-74.local>
2017-03-08 12:32:06 -0700

    added Protocol

--- PizzaProject/ViewController.swift
+++ PizzaProject/ViewController.swift
@@ -7,19 +7,22 @@
 //
 
 import UIKit
+protocol pizzaMaker {
+    func pizzaTime(pizzaria : [Pizzas]) -> Int
+    func createDisaster(pizzas : [Pizzas]) -> Void
+}
+
 
 class ViewController: UIViewController {
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
 }
 
